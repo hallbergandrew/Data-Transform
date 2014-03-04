a8bc6dde123840c4aa69dae7893abce6a0d8d56d
Andrew Hallberg & cody wilson <student@epicodus.com>
2014-03-04 15:12:34 -0800

    finished transform, used each loop on array in hash

--- lib/legacy_data.rb
+++ lib/legacy_data.rb
@@ -1,42 +1,25 @@
 
-def return_value_pair(number)
-scores = { 1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2  => ["D", "G"],
-  3  => ["B", "C", "M", "P"],
-  4  => ["F", "H", "V", "W", "Y"],
-  5  => ["K"],
-  8  => ["J", "X"],
-  10 => ["Q", "Z"]
-}
+def transform_legacy_hash(scrabble_scores)
 
-pairs_new = Hash.new(0)
+new_hash = Hash.new(0)
 
-# scores.each do |key,value|
-#   key_pass = "#{key}".to_i
-#   # puts "#{key}" + "#{index}"
-#   puts "#{key}"
+letters = []
 
-scores.values[number].each {|letters|
-  pairs_new[letters] = scores.keys[number]
-}
 
-# end
-  pairs_new
+scrabble_scores.each do |score, letters|
+  letters.each do |letter|
+    new_hash[letter] = score
+  end
+
+end
+print new_hash
 end
 
-def splitter(data)
-result = []
-keys = data.keys
 
-keys.each do |key, value|
-  result.push(key)
-  result.push(return_value_pair(key))
-end
 
-result
-end
 
-print splitter({ 1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+transform_legacy_hash({ 
+  1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
   2  => ["D", "G"],
   3  => ["B", "C", "M", "P"],
   4  => ["F", "H", "V", "W", "Y"],
@@ -44,4 +27,6 @@ print splitter({ 1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
   8  => ["J", "X"],
   10 => ["Q", "Z"]
 })
+
+
 
